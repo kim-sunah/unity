b5287087bffd7f12f587bc2becef91837dc8ffb3
sunah <1004_sunah@naver.com>
2023-09-19 02:53:59 +0900

    르탄이 넣기, 애니메이션

--- findRtan/Assets/Scripts/gameManager.cs
+++ findRtan/Assets/Scripts/gameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:893d04c380e724106ffce51d58d74186a1b67b2b106a6a5b423f963c34845f08
-size 810
+oid sha256:f6cc6553826f712bb2a4c66ebd18a31fa7f567fcc892f3c334b11628072a9b8b
+size 1162
