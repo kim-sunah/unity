53eb81d0efcb18c2ad0c4a8bc516ca2b26628e7e
sunah <1004_sunah@naver.com>
2023-09-19 03:31:03 +0900

    게임 끝내기

---
+++ findRtan/Assets/Scripts/endTxt.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0807140218d98e02be22d6ddda3709e69ee637b8252588c538af4465a66b3630
+size 409
--- findRtan/Assets/Scripts/gameManager.cs
+++ findRtan/Assets/Scripts/gameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a870559acacb29f8c4138f055de63957c310300b91e19d99fc0b3f800342b59
-size 1959
+oid sha256:6b81d17847ff6e13ff62c6722ccdc37615e1d4e556ddfce59ecc307c275c017a
+size 3116
